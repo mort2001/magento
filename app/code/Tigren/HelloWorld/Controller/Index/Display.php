c4dea975527c2a17e4eb5b5735096f2ce7ea7d28
Mort <thanhdt150@gmail.com>
2022-08-09 12:02:41 +0700

    TIGREN HELLOWORLD
    - Remove some not necessary files
    - Format code

--- app/code/Tigren/HelloWorld/Controller/Index/Display.php
+++ app/code/Tigren/HelloWorld/Controller/Index/Display.php
@@ -9,20 +9,34 @@ namespace Tigren\HelloWorld\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory;
 
 class Display extends Action
 {
+    /**
+     * @var PageFactory
+     */
     protected $_pageFactory;
 
+    /**
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
     public function __construct(
-        Context $context,
-        PageFactory $pageFactory)
+        Context     $context,
+        PageFactory $pageFactory
+    )
     {
         $this->_pageFactory = $pageFactory;
         return parent::__construct($context);
     }
 
+    /**
+     * @return ResponseInterface|ResultInterface|Page
+     */
     public function execute()
     {
         return $this->_pageFactory->create();
