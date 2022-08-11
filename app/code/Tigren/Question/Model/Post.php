70b5bb0bc168d6334bf5b01388be6b4e81f31062
Mort <thanhdt150@gmail.com>
2022-08-11 10:46:20 +0700

    Week2:
    - Create table for
    DB
    - Create Model, ResourceModel, Collection and COntroller to show data on CustomerFront

---
+++ app/code/Tigren/Question/Model/Post.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2022 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Tigren\Question\Model;
+
+use Magento\Framework\Model\AbstractModel;
+
+class Post extends AbstractModel
+{
+    protected function _construct()
+    {
+        $this->_init('Tigren\Question\Model\ResourceModel\Post');
+    }
+}
---
+++ app/code/Tigren/Question/Model/ResourceModel/Post/Collection.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2022 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Tigren\Question\Model\ResourceModel\Post;
+
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+
+class Collection extends AbstractCollection
+{
+    protected $_idFieldName = 'entity_id';
+
+    protected function _construct()
+    {
+        $this->_init('Tigren\Question\Model\Post', 'Tigren\Question\Model\ResourceModel\Post');
+    }
+}
