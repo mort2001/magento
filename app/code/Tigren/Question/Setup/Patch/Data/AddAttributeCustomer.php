26b09a16c56999430f54dd063735bb3ccf70672b
Mort <thanhdt150@gmail.com>
2022-08-19 10:58:02 +0700

    Week2:
    - Update Attribute Customer : done

---
+++ app/code/Tigren/Question/Setup/Patch/Data/AddAttributeCustomer.php
@@ -0,0 +1,105 @@
+<?php
+/*
+ * @author    Tigren Solutions <info@tigren.com>
+ * @copyright Copyright (c) 2022 Tigren Solutions <https://www.tigren.com>. All rights reserved.
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Tigren\Question\Setup\Patch\Data;
+
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Eav\Model\Config;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class AddAttributeCustomer implements DataPatchInterface
+{
+    /**
+     * @var CustomerSetupFactory
+     */
+    private $customerSetupFactory;
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $setup;
+
+    /**
+     * @var Config
+     */
+    private $eavConfig;
+
+    /**
+     * AccountPurposeCustomerAttribute constructor.
+     *
+     * @param  ModuleDataSetupInterface  $setup
+     * @param  CustomerSetupFactory  $customerSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $setup,
+        Config $eavConfig,
+        CustomerSetupFactory $customerSetupFactory
+    ) {
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->setup = $setup;
+        $this->eavConfig = $eavConfig;
+    }
+
+    /** We'll add our customer attribute here */
+    public function apply()
+    {
+        $customerSetup
+            = $this->customerSetupFactory->create(['setup' => $this->setup]);
+        $customerEntity = $customerSetup->getEavConfig()
+            ->getEntityType(Customer::ENTITY);
+        $attributeSetId
+            = $customerSetup->getDefaultAttributeSetId($customerEntity->getEntityTypeId());
+        $attributeGroup
+            = $customerSetup->getDefaultAttributeGroupId(
+            $customerEntity->getEntityTypeId(),
+            $attributeSetId
+        );
+        $customerSetup->addAttribute(Customer::ENTITY, 'is_question_created', [
+            'type'                  => 'int',
+            'input'                 => 'select',
+            'label'                 => 'Question is created',
+            'source'                => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
+            'required'              => true,
+            'default'               => 0,
+            'visible'               => true,
+            'user_defined'          => true,
+            'system'                => false,
+            'is_visible_in_grid'    => true,
+            'is_used_in_grid'       => true,
+            'is_filterable_in_grid' => true,
+            'is_searchable_in_grid' => true,
+            'position'              => 300,
+        ]);
+        $newAttribute = $this->eavConfig->getAttribute(
+            Customer::ENTITY,
+            'is_question_created'
+        );
+        $newAttribute->addData([
+            'used_in_forms'      => [
+                'adminhtml_checkout',
+                'adminhtml_customer',
+                'customer_account_edit',
+                'customer_account_create',
+            ],
+            'attribute_set_id'   => $attributeSetId,
+            'attribute_group_id' => $attributeGroup,
+        ]);
+        $newAttribute->save();
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+}
